70862a40a5ed9d2cfa86f70373e49be03afe4df8
navleensahi <navleenk1@gmail.com>
2019-05-23 13:40:18 -0400

    R2 Green

--- src/Yelling.java
+++ src/Yelling.java
@@ -2,9 +2,15 @@
 public class Yelling {
 
 	public String scream(String name) {
-		
+		if(name == "Peter")
+		{
 		return "Peter is yelling";
 		
 	}
+		else
+		{
+			return "Nobody is yelling";
+			}
+		}
 	
 }
